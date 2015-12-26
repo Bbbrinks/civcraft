59891d419ad4b1cf663d1e3589745997c1a5d949
Bob Brinks <brinks_bob@hotmai.com>
2015-12-26 10:58:12 +0100

    Added a sun and shadows

---
+++ civcraft-core/src/main/java/nl/civcraft/core/rendering/CCDirectionalLightShadowFilter.java
@@ -0,0 +1,25 @@
+package nl.civcraft.core.rendering;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.shadow.DirectionalLightShadowFilter;
+
+/**
+ * Created by Bob on 25-12-2015.
+ * <p>
+ * This is probably not worth documenting
+ */
+public class CCDirectionalLightShadowFilter extends DirectionalLightShadowFilter {
+    /**
+     * Creates a DirectionalLightShadowFilter Shadow Filter More info on the
+     * technique at <a
+     * href="http://http.developer.nvidia.com/GPUGems3/gpugems3_ch10.html">http://http.developer.nvidia.com/GPUGems3/gpugems3_ch10.html</a>
+     *
+     * @param assetManager  the application asset manager
+     * @param shadowMapSize the size of the rendered shadowmaps (512,1024,2048,
+     *                      etc...)
+     * @param nbSplits      the number of shadow maps rendered (the more shadow maps
+     */
+    public CCDirectionalLightShadowFilter(AssetManager assetManager, int shadowMapSize, int nbSplits) {
+        super(assetManager, shadowMapSize, nbSplits);
+    }
+}
